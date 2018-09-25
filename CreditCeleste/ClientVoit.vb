e235d1b3f3540292af353ace32a55aeabaf06b22
Unknown <maartinschultz@gmail.com>
2018-09-25 11:07:13 +0200

    Projet Base

---
+++ CreditCeleste/ClientVoit.vb
@@ -0,0 +1,8 @@
+﻿Imports csClient        'permet de recup le client en c#
+
+Public Class ClientVoit ' declaration classe 
+
+    Inherits Client  ' c'est un héritage ; en c# : Client
+
+
+End Class
